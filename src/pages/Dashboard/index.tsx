da21a5f08439127866055c84983e9c928b52e8bc
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-13 23:19:59 -0300

    Create component Repository

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState, FormEvent, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import { Title, Form, Repositories, Error } from './styles';
 import logoImg from '../../assets/Logo.svg';
 import api from '../../service/api';
@@ -69,7 +70,10 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-          <a key={repository.full_name} href="teste">
+          <Link
+            key={repository.full_name}
+            to={`/repository/${repository.full_name}`}
+          >
             <img
               src={repository.owner.avatar_url}
               alt={repository.owner.login}
@@ -80,7 +84,7 @@ const Dashboard: React.FC = () => {
             </div>
 
             <FiChevronRight size={20} />
-          </a>
+          </Link>
         ))}
       </Repositories>
     </>
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import { useRouteMatch, Link } from 'react-router-dom';
+import logoImg from '../../assets/Logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
+interface Params {
+  repository: string;
+}
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<Params>();
+
+  useEffect(() => {}, []);
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="GitHub Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/64722377?s=460&u=e6f71cb5fdfe19f7730c15e363e642817e97db50&v=4"
+            alt="eu"
+          />
+          <div>
+            <strong>Rocketseat</strong>
+            <p>Descrição</p>
+          </div>
+        </header>
+
+        <ul>
+          <li>
+            <strong>1800</strong>
+            <span>Stars</span>
+          </li>
+
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas </span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="/">
+          <div>
+            <strong>fdfsdfs</strong>
+            <p>dsdfsdds</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
